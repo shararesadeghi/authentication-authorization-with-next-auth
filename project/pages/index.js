26aeb2a97387d0dd860e55a44c819c7382d4820a
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-16 00:12:26 +0330

    signup api created

--- project/pages/index.js
+++ project/pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-    <h1>Nex-Auth</h1>
+    <h1>Next-Auth</h1>
     </div>
   )
 }
