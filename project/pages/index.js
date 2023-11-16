70910e715f7fec49169a47bde09d3100ebb5ba0e
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-16 16:33:30 +0330

    create create a signin page

--- project/pages/index.js
+++ project/pages/index.js
@@ -1,11 +1,16 @@
-import Link from 'next/link';
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-    <h1>Next-Auth</h1>
-    <button><Link href="/signup">Register</Link></button>
+      <h1>Next-Auth</h1>
+      <button>
+        <Link href="/signup">Register</Link>
+      </button>
+      <button>
+        <Link href="/signin">Login</Link>
+      </button>
     </div>
-  )
+  );
 }
