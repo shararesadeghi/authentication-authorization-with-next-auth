46dcfbe969df9a6743802e538b113c448d48d4bf
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-18 00:44:50 +0330

    complete information in dashboard with update -info

--- project/pages/dashboard.js
+++ project/pages/dashboard.js
@@ -1,27 +1,61 @@
+import { useState } from "react";
 import { getSession } from "next-auth/react";
 
-const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    );
+const Dashboard = ({ session }) => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async () => {
+    const res = await fetch("/api/update-info", {
+      method: "POST",
+      body: JSON.stringify({ name, lastName, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+  return (
+    <div>
+      <h1>Your email is :{session.user.email} </h1>
+      <h3>Complete your profile info:</h3>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Repeat password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={submitHandler}>Submit</button>
+    </div>
+  );
 };
 
 export default Dashboard;
 
-export async function getServerSideProps({req}){
-    const session = await getSession({req})
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
 
-    if(!session){
-        return {
-            redirect:{
-                destination:"/signin",
-                permanent:false
-            }
-        }
-    }
-    return{
-        props:{session}
-    }
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
 }
