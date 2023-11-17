e1f03d7946266f6a11170b3dfac48711bcb5a920
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-18 00:00:02 +0330

    show buttons according to authentiactin

--- project/pages/index.js
+++ project/pages/index.js
@@ -1,25 +1,34 @@
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const { status } = useSession();
 
-  const logoutHandler = () =>{
+  const logoutHandler = () => {
     signOut();
-  }
+  };
   return (
     <div className={styles.container}>
       <h1>Next-Auth</h1>
-      <button>
-        <Link href="/dashboard">Dashboard</Link>
-      </button>
-      <button>
-        <Link href="/signup">Register</Link>
-      </button>
-      <button>
-        <Link href="/signin">Login</Link>
-      </button>
-      <button onClick={logoutHandler}>Log Out</button>
+      {status === "authenticated" ? (
+        <>
+          <button>
+            <Link href="/dashboard">Dashboard</Link>
+          </button>
+          <button onClick={logoutHandler}>Log Out</button>
+        </>
+      ) : null}
+      {status === "unauthenticated" ? (
+        <>
+          <button>
+            <Link href="/signup">Register</Link>
+          </button>
+          <button>
+            <Link href="/signin">Login</Link>
+          </button>
+        </>
+      ) : null}
     </div>
   );
 }
