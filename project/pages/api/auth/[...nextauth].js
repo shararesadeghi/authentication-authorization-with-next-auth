eb7a5d3fd1c39fb6d768b7f058b57d9334cc7efa
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-17 22:58:02 +0330

    authorize function is completed

--- project/pages/api/auth/[...nextauth].js
+++ project/pages/api/auth/[...nextauth].js
@@ -1,28 +1,49 @@
 import NextAuth from "next-auth";
 import { CredentialsProvider } from "next-auth/providers";
+import connectDB from "./../../../utils/connectDB";
+import User from "./../../../models/User";
+import { verifyPassword } from "../../../utils/auth";
 
 const authOptions = {
   session: { strategy: "jwt" },
   providers: [
     CredentialsProvider({
-    //   name: "credentials",
-    //   credentails: {
-    //     email: { label: "Email", type: "text", placeholder: "Enter your email" },
-    //     password: {
-    //       label: "Password",
-    //       type: "password",
-    //       placeholder: "Enter your password",
-    //     },
-    //   },
+      //   name: "credentials",
+      //   credentails: {
+      //     email: { label: "Email", type: "text", placeholder: "Enter your email" },
+      //     password: {
+      //       label: "Password",
+      //       type: "password",
+      //       placeholder: "Enter your password",
+      //     },
+      //   },
 
-      async authorize(credentials,req){
-          return {name:"sheri"}
-      }
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+
+        try {
+          await connectDB();
+        } catch (err) {
+          throw new Error("Error in connecting to DB!");
+        }
+
+        if (!email || !password) {
+          throw new Error("Ivalid Data!");
+        }
+
+        const user = await User.findOne({ email: email });
+        if (!user) throw new Error("User does'nt exist!");
+
+        const isValid = await verifyPassword(password, user.password);
+        if (!isValid) throw new Error("Username or password is incorrect!");
+
+        return { email };
+      },
     }),
   ],
-//   pages:{
-//       signIn:"/signin"
-//   }
+  //   pages:{
+  //       signIn:"/signin"
+  //   }
 };
 
 export default NextAuth(authOptions);
